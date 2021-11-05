6d5840f1de774573a90ca0f3d63536161a755acc
srijani07 <srijaniparui07@gmail.com>
2021-11-05 23:03:52 +0530

    Added options and select components

---
+++ script.js
@@ -0,0 +1,38 @@
+const inputAmount = document.querySelector("#input-amount");
+const fromCurrency = document.querySelector("#currency-from-select");
+const toCurrency = document.querySelector("#currency-to-select");
+
+const btnSwap = document.querySelector("#btn-swap");
+const btnConvert = document.querySelector("#btn-convert");
+const divOutput = document.querySelector("#div-output");
+
+var rate;
+fetch("https://open.exchangerate-api.com/v6/latest")
+    .then(response => response.json())
+    .then(result => {
+        rate = result.rates[toCurrency.value]/result.rates[fromCurrency.value];
+    })
+
+function swapCurrency() {
+let temp = fromCurrency.value;
+fromCurrency.value = toCurrency.value;
+toCurrency.value = temp;
+divOutput.innerText = `${inputAmount.value} ${fromCurrency.value} = ${inputAmount.value / rate} ${toCurrency.value}`
+}
+function calculateConvertedCurrency(rate) {
+    
+    divOutput.innerText = `${inputAmount.value} ${fromCurrency.value} = ${inputAmount.value * rate} ${toCurrency.value}`
+}
+
+function validateAmount() {
+    
+}
+
+
+function convertCurrency() {
+        
+    }
+   
+
+btnConvert.addEventListener("click", convertCurrency);
+btnSwap.addEventListener("click", swapCurrency);
